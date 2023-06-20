71ad289eab7ea3ac009b299619f54d9fad9b1380
erika e jeniffer <ejherika@gmail.com>
2023-06-20 09:48:48 -0300

    Segundo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { ListarProdutosComponent } from './listar-produtos/listar-produtos.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'produtos', component:ListarProdutosComponent},
+  {path:'produtos/cadastrar', component:CadastroProdutoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { ListarProdutosComponent } from './listar-produtos/listar-produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CadastroProdutoComponent,
+    ListarProdutosComponent
   ],
   imports: [
     BrowserModule,
